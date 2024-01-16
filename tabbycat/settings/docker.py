7e4740f0ed338cce6c04e2aaf457716956e75964
unknown <mdtanzil858@gmail.com>
2024-01-16 21:11:42 +0600

    Docker redis logic statement removed

--- tabbycat/settings/docker.py
+++ tabbycat/settings/docker.py
@@ -19,8 +19,30 @@ DATABASES = {
     }
 }
 
-if bool(int(os.environ['DOCKER_REDIS'])) if 'DOCKER_REDIS' in os.environ else False:
-    CACHES = {
+# if bool(int(os.environ['DOCKER_REDIS'])) if 'DOCKER_REDIS' in os.environ else False:
+#     CACHES = {
+#         "default": {
+#             "BACKEND": "django_redis.cache.RedisCache",
+#             "LOCATION": "redis://redis:6379/1",
+#             "OPTIONS": {
+#                 "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#                 "SOCKET_CONNECT_TIMEOUT": 5,
+#                 "SOCKET_TIMEOUT": 60,
+#             },
+#         },
+#     }
+
+#     CHANNEL_LAYERS = {
+#         "default": {
+#             "BACKEND": "channels_redis.core.RedisChannelLayer",
+#             "CONFIG": {
+#                 "hosts": [("redis", 6379)],
+#                 "group_expiry": 10800,
+#             },
+#         },
+#     }
+
+CACHES = {
         "default": {
             "BACKEND": "django_redis.cache.RedisCache",
             "LOCATION": "redis://redis:6379/1",
@@ -32,12 +54,12 @@ if bool(int(os.environ['DOCKER_REDIS'])) if 'DOCKER_REDIS' in os.environ else Fa
         },
     }
 
-    CHANNEL_LAYERS = {
-        "default": {
-            "BACKEND": "channels_redis.core.RedisChannelLayer",
-            "CONFIG": {
-                "hosts": [("redis", 6379)],
-                "group_expiry": 10800,
-            },
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("redis", 6379)],
+            "group_expiry": 10800,
         },
-    }
+    },
+}
